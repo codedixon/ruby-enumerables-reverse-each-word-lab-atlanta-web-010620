3b5eac28344501387091db025abc5434ee501085
Rashad Dixon <dixoncodes@gmail.com>
2019-12-26 19:01:26 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(string) 
+  words = string.split(" ")
+  reversed_string = []
+  i = 0
+  while i < words.length
+  reversed_string.push(words[i].reverse)
+  i += 1
+  end
+  answer = reversed_string.join(" ")
+  return answer
+end
